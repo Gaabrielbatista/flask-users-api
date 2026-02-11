f41264349caf22c82eae0430f889661a2dcc92b2
Gaabrielbatista <ggabrielbattista@gmail.com>
2026-02-10 22:28:31 -0300

    refactor: importação 'os' removida

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
-from os import system
-system('clear')
-
-
-from flask import Flask, make_response, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template
 from bd import Carros
 
 app = Flask(__name__)
@@ -17,14 +13,14 @@ def homepage():
 # Endpoint para listar e cadastrar carros
 @app.route('/carros', methods=['GET'])
 def get_carro():
-    return make_response(jsonify(Mensagem="Lista de Carros:", Dados=Carros))
+    return jsonify(Mensagem="Lista de Carros:", Dados=Carros)
 
 @app.route('/carros', methods=['POST'])
 def create_carro():
     carro = request.json
     Carros.append(carro)
 
-    return make_response(jsonify(Mensagem=f"Carro cadastrado com sucesso no ID: {carro['id']}.", Dados=carro))
+    return jsonify(Mensagem=f"Carro cadastrado com sucesso no ID: {carro['id']}.", Dados=carro)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
