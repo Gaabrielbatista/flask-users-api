cf33286b54abe2e2b0f234c79ca90dbf45fad48c
Gaabrielbatista <ggabrielbattista@gmail.com>
2026-02-13 22:25:32 -0300

    fix: conexão com banco correto

--- db/CreateDatabase.sql
+++ db/CreateDatabase.sql
@@ -1,4 +1,4 @@
-USE gaabrielDB;
+USE gaabrieldb;
 
 CREATE TABLE carros (
     id INTEGER NOT NULL auto_increment,
@@ -16,4 +16,4 @@ SET collation_connection = utf8_general_ci;
 INSERT INTO carros (marca, modelo, ano) VALUES ("Fiat", "Marea", 1999);
 INSERT INTO carros (marca, modelo, ano) VALUES ("Fiat", "Uno", 1992);
 INSERT INTO carros (marca, modelo, ano) VALUES ("Chevrolet", "Chevette", 1978);
-INSERT INTO carros (marca, modelo, ano) VALUES ("Volksvagen", "Fusca", 1962);
+INSERT INTO carros (marca, modelo, ano) VALUES ("Volkswagen", "Fusca", 1962);
