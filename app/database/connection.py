1e34061b3b43d51a81bf1ed3a76b5f7a21a443e7
Gaabrielbatista <ggabrielbattista@gmail.com>
2026-03-14 23:58:02 -0300

    style

--- app/database/connection.py
+++ app/database/connection.py
@@ -2,8 +2,10 @@ import mysql.connector
 import os
 from dotenv import load_dotenv
 
+# Carrega as variáveis de ambiente do arquivo .env
 load_dotenv()
 
+# Função para obter a conexão com o banco de dados
 def get_db_connection():
     cnx = mysql.connector.connect(
         user=os.getenv('MYSQL_USER'),
