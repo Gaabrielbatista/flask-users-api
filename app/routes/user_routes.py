a0b3bf234cd92695bc5007f8b0718246b852f827
Gaabrielbatista <ggabrielbattista@gmail.com>
2026-03-17 20:22:31 -0300

    feat: with nas rotas

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import jsonify, request
-from database.connection import get_db_connection
+from database.connection import connection_manager
 from serializers.user_serializer import map_user
 from math import ceil
 
@@ -14,102 +14,72 @@ def get_users():
     
     offset = (page - 1) * limit # offset = quantidade de registros antes da página atual = (página anterior) × limit
 
+    with connection_manager() as cursor:
+        query = 'SELECT id, nome, email, idade FROM users LIMIT %s OFFSET %s;'
+        cursor.execute(query, (limit, offset))
 
-    cnx = get_db_connection()
-    cursor = cnx.cursor()
+        users = cursor.fetchall()
+        users_datas = [map_user(user) for user in users]
 
-    query = 'SELECT id, nome, email, idade FROM users LIMIT %s OFFSET %s;'
-    cursor.execute(query, (limit, offset))
-    users = cursor.fetchall()
+        # Consulta para contar o total de usuários
+        query_count = 'SELECT COUNT(*) FROM users;'
+        cursor.execute(query_count)
 
-    users_datas = [map_user(user) for user in users]
+        total_count = cursor.fetchone()
+        total_count = total_count[0]
 
-    # Consulta para contar o total de usuários
-    query_count = 'SELECT COUNT(*) FROM users;'
-    cursor.execute(query_count)
-    total_count = cursor.fetchone()
-    
-    total_count = total_count[0]
-
-    # page = str(page)
-    # limit = str(limit)
-    # offset = str(offset)
-
-    cursor.close()
-    cnx.close()
+        # page = str(page)
+        # limit = str(limit)
+        # offset = str(offset)
 
     total_pages = ceil(total_count/limit)
 
-    print("page=", page)
+    print("page =", page)
     print("limit=", limit)
-    print("offset=", offset)
+    print("offset =", offset)
 
-    return (jsonify(data=users_datas, pagination={"page":page, "limit":limit, "total":total_count, "total_pages":total_pages}), 200)
+    return jsonify(data=users_datas, pagination={"page":page, "limit":limit, "total":total_count, "total_pages":total_pages}), 200
 
 @app.route('/users/<int:id>', methods=['GET'])
 def get_user(id):
-    cnx = get_db_connection()
-    cursor = cnx.cursor()
+    with connection_manager() as cursor:
 
-    query = 'SELECT id, nome, email, idade FROM users WHERE id = %s'
-    cursor.execute(query, (id,))
+        query = 'SELECT id, nome, email, idade FROM users WHERE id = %s'
+        cursor.execute(query, (id,))
 
-    db_result = cursor.fetchone()
+        db_result = cursor.fetchone()
 
-    if not db_result:
-        cursor.close()
-        cnx.close()
-
-        return (jsonify(error="User not found"), 404)
+        if not db_result:
+            return (jsonify(error="User not found"), 404)
     
-    user_datas = map_user(db_result)
+        user_datas = map_user(db_result)
 
-    cursor.close()
-    cnx.close()
-    return (jsonify(data=user_datas), 200)
+    return jsonify(data=user_datas), 200
 
 @app.route('/users', methods=['POST'])
 def create_user():
     user_datas = request.json
 
-    cnx = get_db_connection()
-    cursor = cnx.cursor()
+    with connection_manager() as cursor:
+        query = 'INSERT INTO users (nome, email, idade) VALUES (%s, %s, %s)'
+        cursor.execute(query, (user_datas["nome"], user_datas["email"], user_datas["idade"]))
 
-    query = 'INSERT INTO users (nome, email, idade) VALUES (%s, %s, %s)'
-    cursor.execute(query, (user_datas["nome"], user_datas["email"], user_datas["idade"]))
-    
-    cnx.commit()
-
-    cursor.close()
-    cnx.close()
-    return jsonify(data=user_datas)  
+    return jsonify(data=user_datas)
 
 @app.route('/users/<int:id>', methods=['PUT'])
 def update_user(id):
     user_datas = request.json
 
-    cnx = get_db_connection()
-    cursor = cnx.cursor()
+    with connection_manager() as cursor:
+        query = 'UPDATE users SET nome = %s, email = %s, idade = %s WHERE id = %s'
+        cursor.execute(query, (user_datas["nome"], user_datas["email"], user_datas["idade"], id))
 
-    query = 'UPDATE users SET nome = %s, email = %s, idade = %s WHERE id = %s'
-    cursor.execute(query, (user_datas["nome"], user_datas["email"], user_datas["idade"], id))
-
-    cnx.commit()
-
-    cursor.close()
-    cnx.close()
     return jsonify(data=user_datas)
 
 @app.route('/users/<int:id>', methods=['DELETE'])
 def delete_user(id):
-    cnx = get_db_connection()
-    cursor = cnx.cursor()
+    with connection_manager() as cursor:
+        query = 'DELETE FROM users WHERE id = %s'
+        cursor.execute(query, (id,))
 
-    query = 'DELETE FROM users WHERE id = %s'
-    cursor.execute(query, (id,))
-
-    cnx.commit()
-
-    cursor.close()
-    cnx.close()
     return jsonify(data=id)
