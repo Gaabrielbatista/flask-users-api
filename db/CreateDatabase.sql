4506391898267412c7d4bd24500c2e07210b004a
Gaabrielbatista <ggabrielbattista@gmail.com>
2026-04-05 18:23:51 -0300

    feat: Null add

--- db/CreateDatabase.sql
+++ db/CreateDatabase.sql
@@ -2,9 +2,9 @@ USE gaabrieldb;
 
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER NOT NULL auto_increment,
-    nome VARCHAR(100),
-    email VARCHAR(100),
-    idade INTEGER,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    idade INTEGER NULL,
     PRIMARY KEY (id)
 );
 
