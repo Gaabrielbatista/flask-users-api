e3217307429db49bd7d85199e09677e9b051a0b7
Gaabrielbatista <ggabrielbattista@gmail.com>
2026-04-05 18:27:22 -0300

    style: map_user

--- app/serializers/user_serializer.py
+++ app/serializers/user_serializer.py
@@ -1,8 +1,8 @@
 # Define a função de mapeamento para o usuário
-def map_user(row):
+def map_user(data):
     return {
-        "id": row[0],
-        "nome": row[1],
-        "email": row[2],
-        "idade": row[3]
+        "id": data[0],
+        "nome": data[1],
+        "email": data[2],
+        "idade": data[3]
     }
